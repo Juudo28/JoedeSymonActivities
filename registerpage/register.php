a3b623ef43873f07168eb99d77045abc017f8153
Juudo28 <143376461+Juudo28@users.noreply.github.com>
2023-12-19 11:38:25 +0800

    Add files via upload

---
+++ registerpage/register.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+
+
+        <link href="https://cdn.jsdelvr.net/npm/bootstrap.min.css" rel="stylesheet" 
+
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-"
+
+        <link rel="stylesheet" href="assets/css/stle.css"/>
+    </head>
+    <body>
+
+
+
+      <!--Navbar-->
+      <nav class="navbar navbar-expand-lgnavbar-light bg-white py-3 fixed-top">
+          <div class="container">
+             <img class="logo" src="assets/imgs/logo.jpeg" />
+             <h2 class="brand">Orange</h2>
+              <button class="navbar-toggler" type="button" data-bs-toggles="collapse" data-bs-target="#navbarSupportedContent">
+               <span class="navbar-toggler-icon"></span> 
+              </button>
+             </div> class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="shop.html">Shop</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <i class="fas fa-shopping-bag"></i>
+                        <i class="fas fa-user"></i>
+                    </li>
+
+
+
+                </ul>
+                
+             </div>
+            </div>
+        </nav>    
+
+<!--Register--> 
+<section class="my-5 py-5">
+ <div class="container text-center mt-3 pt-3">
+     <h2 class="form-weight-bold">Register</h2>
+     <hr class="mx-auto"> 
+ </div>
+ <div class="mx-auto container">
+     <form id="register-form">
+         <div class="form-group">
+             <label>Name</label>
+             <input type="text" class="form-control" id="register-name" name="name" placeholder="Name" required/>
+         </div>
+         <div class="form-group">
+             <label>Email</label>
+             <input type="text" class="form-control" id="register-email" name="email" placeholder="Email" required/>
+         </div>
+         <div class="form-group">
+             <label>Password</label>
+             <input type="password" class="form-control" id="register-password" name="password" placeholder="Password" required/>
+         </div>
+         <div class="form-group">
+             <label>Confirm Password</label>
+             <input type="password" class="form-control" id="register-confirm-password" name="confirmPassword" placeholder="Confirm Password" required/>
+         </div>
+         <div class="form-group">
+             <input type="submit" class="btn" id="register-btn" value="Register"/>
+         </div>
+         <div class="form-group">
+             <a id="login-url" class="btn">Do you have an account? login</a>
+     </form>
+ </div>
+</section>
+    
+    
+<!--Footer-->
+    <footer class="mt-5 py-5">
+        <div class="row container mx-auto pt-5">
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <img class="logo" src="assets/imgs/logo.jpeg"/>
+                <p class="pt-3">We provide the best products for the most affordable prices</p>
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Featured</h5>
+                <ul class="text-uppercase">
+                    <li><a href="#">men</a></li>
+                    <li><a href="#">women</a></li>
+                    <li><a href="#">boys</a></li>
+                    <li><a href="#">girls</a></li>
+                    <li><a href="#">new arrivals</a></li>
+                    <li><a href="#">clothes</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Contact Us</h5>
+            </div>
+               <h6 class="text-uppercase">Address</h6>
+               <p>123 Street Name, City</p>
+             <div>  
+             <div>
+                <h6 class="text-uppercase">Phone</h6>
+                <p>123 456 7890</p>
+             </div>   
+             <div>
+                <h6 class="text-uppercase">Email</h6>
+                <p>info@email.com</p>
+             </div>
+             <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Instagram</h5>
+                <div class="row">
+                    <img src="assests/imgs/featured1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assests/imgs/featured2.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assests/imgs/featured3.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assests/imgs/featured4.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                    <img src="assests/imgs/featured1.jpeg" class="img-fluid w-25 h-100 m-2"/>
+                </div>
+             </div>  
+        </div>
+    </footer>    
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha"
+    </body>
+    <html>
+    
